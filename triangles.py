dc8aac4465486490fea65974bc6acc814b69204c
cryptographer <cryptographer@users.noreply.github.com>
2016-06-23 21:19:17 -0400

    add triangles algorithm

---
+++ triangles.py
@@ -0,0 +1,24 @@
+from PIL import Image, ImageDraw
+
+input_img = Image.open('input.jpg')
+output_img = Image.new(
+    'RGB',
+    (input_img.width, input_img.height),
+    color=(255, 255, 255))
+
+drw = ImageDraw.Draw(output_img)
+side = 32
+
+for x in range(0, input_img.width, side):
+    for y in range(0, input_img.height, side):
+        rgb_one = input_img.getpixel((x, y))
+        rgb_two = input_img.getpixel((x, y + 1))
+
+        drw.polygon(
+            [(x, y), (x + side, y), (x + side/2, y + side)],
+            fill=rgb_one)
+        drw.polygon(
+            [(x - side/2, y + side), (x, y), (x + side/2, y + side)],
+            fill=rgb_two)
+
+output_img.save('output.jpg')
