ab7636850cbca61b759e5ba2308a79d2cac2765a
cryptographer <cryptographer@users.noreply.github.com>
2016-06-28 17:48:53 -0400

    handle edge case

--- triangles.py
+++ triangles.py
@@ -1,24 +1,35 @@
 from PIL import Image, ImageDraw
 
-input_img = Image.open('input.jpg')
-output_img = Image.new(
-    'RGB',
-    (input_img.width, input_img.height),
-    color=(255, 255, 255))
 
-drw = ImageDraw.Draw(output_img)
-side = 32
+def main():
+    input_img = Image.open('input.jpg')
+    output_img = Image.new(
+        'RGB',
+        (input_img.width, input_img.height),
+        color=(255, 255, 255))
 
-for x in range(0, input_img.width, side):
-    for y in range(0, input_img.height, side):
-        rgb_one = input_img.getpixel((x, y))
-        rgb_two = input_img.getpixel((x, y + 1))
+    drw = ImageDraw.Draw(output_img)
+    side = 32
 
-        drw.polygon(
-            [(x, y), (x + side, y), (x + side/2, y + side)],
-            fill=rgb_one)
-        drw.polygon(
-            [(x - side/2, y + side), (x, y), (x + side/2, y + side)],
-            fill=rgb_two)
+    for x in range(0, input_img.width, side):
+        for y in range(0, input_img.height, side):
+            rgb_one = input_img.getpixel((x, y))
+            rgb_two = input_img.getpixel((x, y + 1))
 
-output_img.save('output.jpg')
+            drw.polygon(
+                [(x, y), (x + side, y), (x + side/2, y + side)],
+                fill=rgb_one)
+            drw.polygon(
+                [(x - side/2, y + side), (x, y), (x + side/2, y + side)],
+                fill=rgb_two)
+
+            if (input_img.width - x) <= side:
+                drw.polygon(
+                    [(x + side/2, y + side), (x + side, y), (x + 3*side/2, y + side)],
+                    fill=rgb_two)
+
+    output_img.save('output.jpg')
+
+
+if __name__ == "__main__":
+    main()
